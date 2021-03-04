63d987f1470d3655129a46ad59ce3cf86d75ffd5
Hans <lgs840522@gmail.com>
2021-03-05 01:10:34 +0800

    feat(client): update hard code string to formatMessages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Chatcord Bot';
+
 // Run when client connects
 io.on('connection', socket => {
   // Welcome current user
-  socket.emit('message', 'Welcome to The Realtime chatroom!')  // this is to the single client 
+  socket.emit('message', formatMessage(botName, 'Welcome to The Realtime chatroom!'))  // this is to the single client 
 
   // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');   // this is to all the clients except the client that's connecting
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));   // this is to all the clients except the client that's connecting
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');  // this is to all the clients
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));  // this is to all the clients
   });
 
   // Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg)
+    io.emit('message', formatMessage('USER', msg))
   });
 
 }); 
